22f8e4418ae67b1285a29a225b9767aebdeb6746
Your Name <you@example.com>
2025-06-03 01:45:43 -0700

    Initial commit: NestJS project setup

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,5 @@
+import { CreateUserDto } from '../dto/create-user.dto';
+
+export class User extends CreateUserDto {
+  id: number;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  private users: User[] = [];
+
+  create(createUserDto: CreateUserDto) {
+    const user = { id: Date.now(), ...createUserDto } as User;
+    this.users.push(user);
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return this.users.find(user => user.id === id);
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index === -1) return null;
+    this.users[index] = { ...this.users[index], ...updateUserDto };
+    return this.users[index];
+  }
+
+  remove(id: number) {
+    const index = this.users.findIndex(user => user.id === id);
+    if (index === -1) return null;
+    return this.users.splice(index, 1);
+  }
+}
